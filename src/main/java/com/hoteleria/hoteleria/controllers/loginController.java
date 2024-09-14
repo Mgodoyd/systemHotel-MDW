47a2c7b477f523bb95b896950352d481248d3f44
Mgodoyd <godoymario110@gmail.com>
2024-09-14 17:04:06 -0600

    Change version Java

--- src/main/java/com/hoteleria/hoteleria/controllers/loginController.java
+++ src/main/java/com/hoteleria/hoteleria/controllers/loginController.java
@@ -1,29 +1,18 @@
 package com.hoteleria.hoteleria.controllers;
 
-import java.lang.foreign.Linker.Option;
 import java.util.Map;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.hoteleria.hoteleria.dtos.staffDto;
 import com.hoteleria.hoteleria.helpers.responseHelper;
-import com.hoteleria.hoteleria.models.personal;
 import com.hoteleria.hoteleria.services.personalService;
 import com.hoteleria.hoteleria.services.auth.AuthenticationRequest;
 import com.hoteleria.hoteleria.services.auth.AuthenticationService;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @CrossOrigin(origins = { "*", "https://localhost/", "http://localhost:8100" }, methods = { RequestMethod.POST,
@@ -31,42 +20,48 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/api/v1")
 public class loginController {
 
-    private final AuthenticationService authenticationService;
+    @Autowired
+    private AuthenticationService authenticationService;
 
     @Autowired
     private personalService userService;
 
-    public loginController(AuthenticationService authenticationService) {
-        this.authenticationService = authenticationService;
-    }
+    // public loginController(AuthenticationService authenticationService) {
+    // this.authenticationService = authenticationService;
+    // }
 
-    @PostMapping("/register") // Register a new user
-    public ResponseEntity<responseHelper<Object>> register(@RequestBody personal user) {
-        try {
+    // @PostMapping("/register") // Register a new user
+    // public ResponseEntity<responseHelper<Object>> register(@RequestBody personal
+    // user) {
+    // try {
 
-            staffDto existingEmail = userService.findByEmail(user.getEmail()).orElse(null);
-            if (existingEmail == null) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body(new responseHelper<>("Error", HttpStatus.BAD_REQUEST, null, "Email already exists"));
-            }
+    // staffDto existingEmail =
+    // userService.findByEmail(user.getEmail()).orElse(null);
+    // if (existingEmail == null) {
+    // return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+    // .body(new responseHelper<>("Error", HttpStatus.BAD_REQUEST, null, "Email
+    // already exists"));
+    // }
 
-            staffDto existingUsers = userService.findByTelefono(user.getPhone());
-            if (existingUsers != null) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body(new responseHelper<>("Error", HttpStatus.BAD_REQUEST, null, "Username already exists"));
-            }
+    // staffDto existingUsers = userService.findByTelefono(user.getPhone());
+    // if (existingUsers != null) {
+    // return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+    // .body(new responseHelper<>("Error", HttpStatus.BAD_REQUEST, null, "Username
+    // already exists"));
+    // }
 
-            personal createdUser = authenticationService.register(user);
+    // personal createdUser = authenticationService.register(user);
 
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(new responseHelper<>("User registrated successfully", HttpStatus.OK,
-                            createdUser, null));
+    // return ResponseEntity.status(HttpStatus.OK)
+    // .body(new responseHelper<>("User registrated successfully", HttpStatus.OK,
+    // createdUser, null));
 
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new responseHelper<>("Error", HttpStatus.INTERNAL_SERVER_ERROR, null, e));
-        }
-    }
+    // } catch (Exception e) {
+    // return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+    // .body(new responseHelper<>("Error", HttpStatus.INTERNAL_SERVER_ERROR, null,
+    // e));
+    // }
+    // }
 
     @PostMapping("/login") // Login a user
     public ResponseEntity<responseHelper<Object>> authenticate(@RequestBody AuthenticationRequest request) {
